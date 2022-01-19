714af33a1de169f6ce5161cdac80c3e34b61e166
matheusjean <matheusjean11@hotmail.com>
2022-01-19 16:12:06 -0300

    chore: adicionado dependência de typeorm e pg

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
