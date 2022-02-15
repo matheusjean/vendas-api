14e93b869a99ec7344c95f7502c8189c0ff75477
matheusjean <matheusjean11@gmail.com>
2022-02-15 15:20:37 -0300

    feat: exibir produto pelo id

---
+++ src/modules/products/service/ShowProductService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/appError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/productsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+export default class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Produto não encontrado');
+    }
+
+    return product;
+  }
+}
