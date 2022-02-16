58bc1985d437cfa64976543e9edd800441e44744
matheusjean <matheusjean11@gmail.com>
2022-02-16 12:11:42 -0300

    feat: rotas de controle

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
+import productsRouter from '@modules/products/routes/Products.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
+routes.use('/products', productsRouter);
+
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Api na versão 1.0' })
-})
+  return res.json({ message: 'Api na versão 1.0' });
+});
 
 export default routes;
