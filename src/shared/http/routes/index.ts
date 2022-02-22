803a9801d57098cd104af529383b93a4664b2d82
matheusjean <matheusjean11@gmail.com>
2022-02-22 14:09:44 -0300

    feat: rotas de sessão

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/Products.routes';
+import sessionRouter from '@modules/users/routes/Sessions.routes';
 import usersRouter from '@modules/users/routes/Users.routes';
 import { Router } from 'express';
 
@@ -6,5 +7,6 @@ const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
